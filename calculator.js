17227f49b330aca76158269a733c73622c7df9cb
Abby sWagner <awagner.sh@mysummitps.org>
2016-11-07 21:02:54 +0000

    update

--- calculator.js
+++ calculator.js
@@ -5,9 +5,9 @@ var secondNumber = document.getElementById("input2");
 var calculatorbutton = document.getElementById("calculatorbutton");
 
 
-
+//the following is what action will happen
 function calculate() {
-    
+    // I am creating more variables that are connected to the value as well
     var num1 = firstNumber.value;
     var num2 = secondNumber.value;
     var answer = document.getElementById("answer");
@@ -15,39 +15,47 @@ function calculate() {
     answer.innerHTML = num1;*/
     /*
         answer.style.backgroundColor = "red"*/
-
+        
+// if statement for when the operation is addition
     if (operation.value === "+") {
         if (num1 == "2" && num2 == "2") {
         answer.innerHTML = "Fish"
         }
         
         else if (num1 == "1234567890" && num2 == "1234567890") {
-        answer.innerHTML = "I'm too lazy"
+        answer.innerHTML = "I'm too lazy";
         }
         
         else {
         answer.innerHTML = Number(num1) + Number(num2);
         }
     }
-
+//if statement when operation is substraction
     else if (operation.value === "-") {
         if (num1 == "1" && num2 == "1") {
-            answer.innerHTML = "You should already know this, you dummy"
+            answer.innerHTML = "You should already know this, you dummy";
         }
         
         else {
         answer.innerHTML = Number(num1) - Number(num2);
         }
     }
+    //if statement when operation is division
     else if (operation.value === "/") {
         answer.innerHTML = Number(num1) / Number(num2);
     }
+    
+    //if statement when operation is multiplication
     else if (operation.value === "*") {
         answer.innerHTML = Number(num1) * Number(num2);
     }
+    
+    //if statement when operation pertains to remainder
     else if (operation.value === "%") {
         answer.innerHTML = Number(num1) % Number(num2);
     }
+    
+    //if statement when operation is whether they are equal or not
     else if (operation.value === "==") {
         if (num1 === num2) {
             answer.innerHTML = "true";
@@ -59,5 +67,5 @@ function calculate() {
     }
 }
 
-
+//says the function will run when you click the calculator button
 calculatorbutton.addEventListener("click", calculate);
